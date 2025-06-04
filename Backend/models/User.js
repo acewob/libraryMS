fd403cb3064f1518efcc803c4e794d852a00dd3c
acewob <alexwoby@gmail.com>
2025-06-04 19:07:06 +0300

    added graphql yoga

--- Backend/models/User.js
+++
@@ -1,6 +0,0 @@
-import mongoose from"mongoose";
-
-export const userSchema=new mongoose.Schema({
-    name:String,
-    age:String,
-})
--- Backend/resolver/resolver.js
+++
@@ -1,8 +0,0 @@
-const user=require('../models/User')
-
-const resolver={
-    user:()=>{
-        return "Hello from user"
-    }
-}
-module.exports=resolver
