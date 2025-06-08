72e86aec531da4d460561580a84c17c263f18058
acewob <alexwoby@gmail.com>
2025-06-08 13:31:53 +0300

    User CRUD operations

--- Backend/src/graphql/models/usermodel.js
+++ Backend/src/graphql/models/usermodel.js
@@ -1,45 +1,61 @@
+import { ObjectId } from "mongodb";
+
 export const typeDefs=`
 type Query{
-    user:User
+    users:[User!]!
+    user(id:ID!):User
 }
 type Mutation{
     createUser(user:NewUserInput!):User
+    updateUser(id:ID!,update:UpdateUserInput):User
 }
 
 
 input NewUserInput{
     name:String!
-    age:Int!
+    email:String!
+}
+input UpdateUserInput{
+    name:String!
 }
 
-
 type User{
-    id:Int
+    id:ID!
     name:String
-    age:Int
+    email:String
 }
 `
 
 export const resolvers={
     Query:{
-        user:()=>{
-            return{
-                id:1,
-                name:"buddy"
-            }
-        }
+        users:(_,args,{mongo})=>{
+            return mongo.users.find().toArray();
+        },
+        user:async (obj,{id},{mongo})=>{
+            return mongo.users.findOne({_id: new ObjectId(id)});
+        },
     },
     Mutation:{
         createUser:async(_,{user},{mongo})=>{
-            const users=await mongo.users.find().toArray();
-            console.log(users);
+            const response=await mongo.users.insertOne(user);
             return{
-                id:2,
+                id:response.insertedId,
                 ...user,
             }
+        },
+        updateUser: async (obj,{id,update},{mongo})=>{
+            console.log(update)
+            await mongo.users.updateOne(
+                {_id:new ObjectId(id)},
+                {$set:{
+                    name:update.name
+                }}
+            );
+            return await mongo.users.findOne({_id: new ObjectId(id)});
         }
     },
     User:{
+        id:({_id,id})=>_id||id,
         name:(obj)=>{
             return obj.name.trim().toUpperCase();
         }
